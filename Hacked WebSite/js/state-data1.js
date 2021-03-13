5f9537f5ea806b927287448e78e78e8c9c1d4873
LinxinJiang <70239443+LinxinJiang@users.noreply.github.com>
2021-03-13 07:28:22 -0800

    Add files via upload

---
+++ Hacked WebSite/js/state-data1.js
@@ -0,0 +1,38 @@
+var stateData = {
+	cat1: {
+		name: " ",
+		weight: '2 lbs',
+		age: '6 months',
+		color: 'White',
+	},
+	cat2: {
+		name: " ",
+		weight: '13 oz',
+		age: '2 months',
+		color: 'Grey and White',
+	},
+	cat3: {
+		name: " ",
+		weight: '4 lbs',
+		age: '2 years',
+		color: 'Orange',
+	},
+	dog1: {
+		name: " ",
+		weight: '22 lbs',
+		age: '6 year',
+		color: 'Brown',
+	},
+	dog2: {
+		name: " ",
+		weight: '6 lbs',
+		age: '12 months',
+		color: 'White, Brown, and Black',
+	},
+	dog3: {
+		name: " ",
+		weight: '5 lbs',
+		age: '11 months',
+		color: 'Brown and White',
+	}
+};
---
+++ Hacked WebSite/readme.md
@@ -0,0 +1,11 @@
+What we've done:
+  changed state facts drop down to show animal stats
+    -need to update images
+  changed the js file from state facts to specifics about the animal
+  modified the form at the bottom
+  changed history text
+  center the : main role="main"
+
+
+  changed background color
+  changed inner wrapper color
