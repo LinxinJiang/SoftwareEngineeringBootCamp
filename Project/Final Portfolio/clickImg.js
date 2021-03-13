319e83f9d2900c3aede026ea7edc1a59e1c409fd
LinxinJiang <70239443+LinxinJiang@users.noreply.github.com>
2021-03-13 13:27:06 -0800

    Add files via upload

---
+++ Project/Final Portfolio/clickImg.js
@@ -0,0 +1,26 @@
+$(function(){
+  $("#lab10").click(function(){
+    $("#lab10img").attr('src','lab10.gif');
+  });
+
+  $("#lab11").click(function(){
+    $("#lab11img").attr('src','lab11.gif');
+  });
+
+  $("#lab12").click(function(){
+    $("#lab12img").attr('src','lab12.gif');
+  });
+
+  $("#lab13").click(function(){
+    $("#lab13img").attr('src','lab13.gif');
+  });
+
+  $("#lab14").click(function(){
+    $("#lab14img").attr('src','lab14.gif');
+  });
+
+  $("#lab15").click(function(){
+    $("#lab15img").attr('src','lab15.gif');
+  });
+
+});
